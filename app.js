55c4d4511926772c852ffbb3df25b1c79aa44fcd
BardhShabanii <bardhi0105@gmail.com>
2023-02-09 02:58:59 +0100

    added db uri/connected to mongodb, added a route for testing, added index view for testing

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const app = express();
 const bookRoutes = require("./routes/bookRoutes");
 const methodOverride = require("method-override");
-
-
+const dbURI = "mongodb+srv://bardhi:bardhi123@cluster0.4rotd4t.mongodb.net/?retryWrites=true&w=majority"
 const PORT = 8000;
+
+mongoose.connect(dbURI);
+
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,1 +1,7 @@
-// const bookController
+const bookController = require("../controllers/bookController");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", bookController.getBooks);
+
+module.exports = router;
